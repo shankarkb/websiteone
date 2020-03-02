bb5191ade98c4884a5f5ea61bf3ce55e86036db8
shankarkb <53208450+shankarkb@users.noreply.github.com>
2020-03-02 18:35:32 +0530

    initial updates

---
+++ public/js/scripts.js
@@ -0,0 +1,4 @@
+const selectElement = (element) => document.querySelector(element);
+selectElement('.menu-icons').addEventListener('click', ()=>{
+    selectElement('nav').classList.toggle('active');
+});
